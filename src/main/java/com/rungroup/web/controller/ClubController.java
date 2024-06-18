26dcdb1b5d463e08f26defda30f89274f74d5f21
Luis <luisantoniodiaz97@gmail.com>
2024-06-18 16:08:15 -0600

    Update: "Details"

--- src/main/java/com/rungroup/web/controller/ClubController.java
+++ src/main/java/com/rungroup/web/controller/ClubController.java
@@ -3,14 +3,17 @@ package com.rungroup.web.controller;
 import com.rungroup.web.dto.ClubDto;
 import com.rungroup.web.models.Club;
 import com.rungroup.web.service.ClubService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.naming.Binding;
 import java.util.List;
 
 @Controller
@@ -29,6 +32,13 @@ public class ClubController {
         return "club-list";
     }
 
+    @GetMapping("/clubs/{clubId}")
+    public String clubDetail(@PathVariable("clubId")Long clubId, Model model){
+        ClubDto clubDto = clubService.findClubById(clubId);
+        model.addAttribute("club", clubDto);
+        return  "club-detail";
+    }
+
     @GetMapping("/clubs/new")
     public String createClubForm(Model model) {
         Club club = new Club();
@@ -37,8 +47,12 @@ public class ClubController {
     }
 
     @PostMapping("/clubs/new")
-    public String saveClub(@ModelAttribute("club") Club club) {
-        clubService.saveClub(club);
+    public String saveClub(@Valid @ModelAttribute("club") ClubDto clubDto, BindingResult result, Model model) {
+        if(result.hasErrors()){
+            model.addAttribute("club", clubDto);
+            return "club-create";
+        }
+        clubService.saveClub(clubDto);
         return "redirect:/clubs";
     }
 
@@ -50,7 +64,10 @@ public class ClubController {
     }
 
     @PostMapping("/clubs/{clubId}/edit")
-    public String updateClub(@PathVariable("clubId") Long clubId, @ModelAttribute("club") ClubDto club){
+    public String updateClub(@PathVariable("clubId") Long clubId, @Valid @ModelAttribute("club") ClubDto club, BindingResult result){
+        if(result.hasErrors()){
+            return "club-edit";
+        }
         club.setId(clubId);
         clubService.updateClub(club);
         return "redirect:/clubs";
--- src/main/java/com/rungroup/web/dto/ClubDto.java
+++ src/main/java/com/rungroup/web/dto/ClubDto.java
@@ -1,5 +1,6 @@
 package com.rungroup.web.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,8 +10,11 @@ import java.time.LocalDateTime;
 @Builder
 public class ClubDto {
     private Long id;
+    @NotEmpty(message = "Club Title should not be empty")
     private String title;
+    @NotEmpty(message = "Club Photo Link should not be empty")
     private String photoUrl;
+    @NotEmpty(message = "Club Content should not be empty")
     private String content;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
