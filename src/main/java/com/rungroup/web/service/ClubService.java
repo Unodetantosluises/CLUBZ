c70aed26d790bba3cd98c50e33d97c3f5fd1bc8b
Luis <luisantoniodiaz97@gmail.com>
2024-06-18 12:01:13 -0600

    Update: Edit Component

--- src/main/java/com/rungroup/web/service/ClubService.java
+++ src/main/java/com/rungroup/web/service/ClubService.java
@@ -1,4 +1,5 @@
 package com.rungroup.web.service;
+
 import com.rungroup.web.dto.ClubDto;
 import com.rungroup.web.models.Club;
 
@@ -8,4 +9,8 @@ public interface ClubService {
     List<ClubDto> findAllClubs();
 
     Club saveClub(Club club);
+
+    ClubDto findClubById(Long clubId);
+
+    void updateClub(ClubDto clubDto);
 }
